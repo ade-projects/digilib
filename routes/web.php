2ed208b2090b2743a0acbbf7d5820fccc3f34641
ade-projects <projectalpha774@gmail.com>
2026-01-30 23:14:38 +0700

    feath(routes): tambah middleware mencegah akses ilegal

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,30 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
+// Public route
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+// Redirect route
+Route::get('/',function () {
+    if (Auth::check()) {
+        return redirect('/dashboard');
+    }
+    return redirect('/login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+// Protected access
+Route::middleware(['auth'])->group(function () {
+
+    // 1. Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // 2. Logout
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 });
